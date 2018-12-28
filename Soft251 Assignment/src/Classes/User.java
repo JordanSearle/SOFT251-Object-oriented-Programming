5bfe3a34f7a2052d475984db3eaf1d6d50dbb612
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2018-12-28 13:27:41 +0000

    Started to create Java Library
    Created abstract User Class
    - Added Strings
    - Created Getter and Setter Methods
    Created Patient Class
    - Extends User Class

---
+++ Soft251 Assignment/src/Classes/User.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Jordan
+ */
+public abstract class User {
+    private String forename;
+    private String surname;
+    private String addressLineOne;
+    private String addressLineTwo;
+    private String postcode;
+    private String iDnum;
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddressLineOne() {
+        return addressLineOne;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        this.addressLineOne = addressLineOne;
+    }
+
+    public String getAddressLineTwo() {
+        return addressLineTwo;
+    }
+
+    public void setAddressLineTwo(String addressLineTwo) {
+        this.addressLineTwo = addressLineTwo;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getiDnum() {
+        return iDnum;
+    }
+
+    public void setiDnum(String iDnum) {
+        this.iDnum = iDnum;
+    }
+}
